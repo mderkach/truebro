d8aadd6f3360654ce71262fce171148023c370ac
skeev_ <derkach94@gmail.com>
2020-11-09 16:03:58 +0300

    index page init

---
+++ src/views/pages/index/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+// page components
+import button from '../../components/button/button';
+import inputSearch from '~/views/components/input/inputSearch';
+// page styles
+import './index.scss';
+// init
+inputSearch.init();
