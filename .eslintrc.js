2b65c1ffc92ae56bfb5663c445832d0dab7d7218
skeev_ <derkach94@gmail.com>
2020-11-09 17:36:35 +0300

    react table init

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,8 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: ['airbnb', 'prettier'],
-  plugins: ['babel', 'import', 'prettier'],
+  extends: ['airbnb', 'plugin:react/recommended', 'prettier'],
+  plugins: ['babel', 'import', 'react', 'prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -14,10 +14,14 @@ module.exports = {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 2018,
+    ecmaVersion: 2021,
     sourceType: 'module',
   },
   settings: {
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
     'import/resolver': {
       webpack: {
         config: './build/webpack.base.conf.js',
@@ -32,5 +36,22 @@ module.exports = {
     'no-process-exit': 'off',
     'object-shorthand': 'off',
     'class-methods-use-this': 'off',
+    semi: 1,
+    eqeqeq: 'off',
+    'react/jsx-filename-extension': [0],
+    'import/extensions': 'off',
+    'import/no-unresolved': [2, { caseSensitive: false }],
+    'react/jsx-props-no-spreading': 'off',
+    'react/prop-types': 'off',
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'global-require': 'off',
+    'react/jsx-max-props-per-line': [
+      1,
+      {
+        maximum: 1,
+      },
+    ],
+    'jsx-a11y/tabindex-no-positive': 'off',
   },
 };
--- src/views/pages/index/index.js
+++ src/views/pages/index/index.js
@@ -1,8 +1,99 @@
 /* eslint-disable no-unused-vars */
+// page core
+import React from 'react';
+import ReactDOM from 'react-dom';
 // page components
 import button from '../../components/button/button';
 import inputSearch from '~/views/components/input/inputSearch';
 // page styles
 import './index.scss';
+
+// page custom scripts
+const reactNode = document.querySelector('.index-table__container');
+
+const headers = [
+  {
+    name: 'Бренд',
+    align: 'left',
+    sortable: false,
+  },
+  {
+    name: 'Возраст на рынке г.',
+    align: 'right',
+    sortable: true,
+  },
+  {
+    name: 'Для новичков',
+    align: 'center',
+    sortable: false,
+  },
+  {
+    name: 'Обработка претензий %',
+    align: 'right',
+    sortable: true,
+  },
+  {
+    name: 'Позитивных отзывов %',
+    align: 'right',
+    sortable: true,
+  },
+  {
+    name: 'Удобство платежей 0-10',
+    align: 'right',
+    sortable: true,
+  },
+  {
+    name: 'Мин депозит',
+    align: 'right',
+    sortable: true,
+  },
+  {
+    name: 'Бонус на депозит',
+    align: 'right',
+    sortable: true,
+  },
+  {
+    name: 'Узнаваемость Бренда 0-10',
+    align: 'right',
+    sortable: true,
+  },
+  {
+    name: 'Добавить к сравнению',
+    align: 'center',
+    sortable: false,
+  },
+];
+
+const Table = () => {
+  return (
+    <>
+      <div className="index-table__outer">
+        <div className="index-table__header index-table__row">
+          {headers.map((elem) => (
+            <div
+              key={elem.name}
+              className="index-table__cell text-small"
+              style={{ textAlign: elem.align }}
+            >
+              {elem.name}
+              {elem.sortable && (
+                <svg className="index-table__sort">
+                  <use xlinkHref="./assets/img/svg/sprite.svg#sort-icon" />
+                </svg>
+              )}
+            </div>
+          ))}
+        </div>
+        <div className="index-table__filter">filter</div>
+        <div className="index-table__rows index-table__row">rows</div>
+      </div>
+    </>
+  );
+};
+
+if (reactNode) {
+  ReactDOM.render(<Table />, reactNode);
+}
+
 // init
 inputSearch.init();
