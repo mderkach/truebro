0150726915f98a109718fd303d87e3c2e1648bb1
skeev_ <derkach94@gmail.com>
2021-01-11 13:07:14 +0300

    broker tablet & desktop render

--- src/modules/Broker/Components/BrokerPaymentSystems/BrokerPaymentSystems.jsx
+++ src/modules/Broker/Components/BrokerPaymentSystems/BrokerPaymentSystems.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useMediaQuery } from 'react-responsive';
 // utils
 import classNames from 'classnames/bind';
 import useToggleHidden from '~u/useToggleHidden';
@@ -23,16 +24,30 @@ const BrokerPaymentSystems = (props) => {
     e.preventDefault();
     toggleHiddenEvent();
   };
+
+  const isDesktop = useMediaQuery({
+    query: '(min-width: 1366px)',
+  });
+
+  const isTablet = useMediaQuery({
+    query: '(min-width: 768px) and (max-width: 1365px)',
+  });
+
   return (
     <>
       <div className={classes(styles.Wrapper, className)}>
         <h3 className="h3 medium">Платежные системы</h3>
         <div className={styles.PictureWrapper}>
-          {items.map(
-            (i, index) => index < 12 && <Picture key={i.src} src={i.src} cls={styles.Picture} />,
-          )}
+          {isDesktop &&
+            items.map(
+              (i, index) => index < 12 && <Picture key={i.src} src={i.src} cls={styles.Picture} />,
+            )}
+          {isTablet &&
+            items.map(
+              (i, index) => index < 14 && <Picture key={i.src} src={i.src} cls={styles.Picture} />,
+            )}
         </div>
-        {items.length > 11 && (
+        {isDesktop && items.length > 11 && (
           <>
             <div className={classes(styles.PictureHiddenWrapper, styles.PictureWrapper)}>
               {items.map(
@@ -47,6 +62,25 @@ const BrokerPaymentSystems = (props) => {
               ref={hiddenTriggerRef}
             >
               <span ref={spanRef}>{`показать еще ${items.length - 12}`}</span>
+              <Icon cls={styles.PictureHiddenTriggerIcon} name="chevron-down-icon" />
+            </button>
+          </>
+        )}
+        {isTablet && items.length > 14 && (
+          <>
+            <div className={classes(styles.PictureHiddenWrapper, styles.PictureWrapper)}>
+              {items.map(
+                (i, index) =>
+                  index > 13 && <Picture key={i.src} src={i.src} cls={styles.Picture} />,
+              )}
+            </div>
+            <button
+              type="button"
+              className={hiddenTriggerClass}
+              onClick={toggleHidden}
+              ref={hiddenTriggerRef}
+            >
+              <span ref={spanRef}>{`показать еще ${items.length - 14}`}</span>
               <Icon cls={styles.PictureHiddenTriggerIcon} name="chevron-down-icon" />
             </button>
           </>
