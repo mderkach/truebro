24950fb6956caad92d3d6dc4558e42054bd2a266
skeev_ <derkach94@gmail.com>
2021-01-12 13:47:40 +0300

    button teriary variant

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+// utils
 import classNames from 'classnames/bind';
 // styles
 import styles from './Button.local';
@@ -6,13 +8,15 @@ import styles from './Button.local';
 const classes = classNames.bind(styles);
 
 const Button = (props) => {
-  const { children, cls, type, href, variant = 'primary', text, ...rest } = props;
+  const { children, cls, type = 'button', href, variant = 'primary', text, ...rest } = props;
 
   const btnClass = classes({
     'text-small': true,
+    'text-regular': type === 'router',
     medium: variant !== 'chip',
-    ButtonPrimary: variant === 'primary',
+    ButtonPrimary: variant === 'primary' && type !== 'router',
     ButtonSecondary: variant === 'secondary',
+    ButtonTertiary: variant === 'tertiary' || type === 'router',
     ButtonChip: variant === 'chip',
     [cls]: cls,
   });
@@ -21,21 +25,27 @@ const Button = (props) => {
     <>
       {type === 'submit' && (
         <button type="submit" className={btnClass} {...rest}>
-          <span>{text}</span>
+          {text && <span>{text}</span>}
           {children}
         </button>
       )}
       {type === 'link' && (
         <a href={href} className={btnClass} {...rest}>
-          <span>{text}</span>
+          {text && <span>{text}</span>}
           {children}
         </a>
       )}
       {type === 'button' && (
         <button type="button" className={btnClass} {...rest}>
-          <span>{text}</span>
+          {text && <span>{text}</span>}
           {children}
         </button>
+      )}
+      {type === 'router' && (
+        <Link className={btnClass} {...rest}>
+          {text && <span>{text}</span>}
+          {children}
+        </Link>
       )}
     </>
   );
