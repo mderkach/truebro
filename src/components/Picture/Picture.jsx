ad83824a68648590021477a9334aeae1071b71fa
skeev_ <derkach94@gmail.com>
2021-01-14 13:28:41 +0300

    Picture: support .webp, styles adjustment

--- src/components/Picture/Picture.jsx
+++ src/components/Picture/Picture.jsx
@@ -1,25 +1,42 @@
 import React from 'react';
+// styles
+import styles from './Picture.local';
 
 const Picture = (props) => {
   const { cls, alt, srcset, src, media, ...rest } = props;
 
-  let picname;
-  if (srcset) picname = srcset.substring(srcset.lastIndexOf('/') + 1, srcset.lastIndexOf('.'));
+  const picname = (path) => path.substring(path.lastIndexOf('/') + 1, path.lastIndexOf('.'));
+  const pathName = (path) => path.substring(0, path.lastIndexOf('/') + 1);
+  const ext = (path) => path.substring(path.lastIndexOf('.'));
 
   return (
     <picture className={cls} {...rest}>
       {!media && (
         <>
-          <source srcSet={src || srcset} />
-          <img src={src || `./assets/img/${picname}.webp`} alt={alt || ''} />
+          <source
+            srcSet={`${pathName(src || srcset)}${picname(src || srcset)}${ext(src || srcset)}`}
+          />
+          <img
+            className={styles.Img}
+            src={`${pathName(src || srcset)}${picname(src || srcset)}.webp`}
+            alt={alt || ''}
+          />
         </>
       )}
       {media && (
         <>
           {media.map((s) => (
-            <source key={s.media} srcSet={s.src} media={s.media} />
+            <source
+              key={s.media}
+              srcSet={`${pathName(s.src)}${picname(s.src)}${ext(s.src)}`}
+              media={s.media}
+            />
           ))}
-          <img src={src} alt={alt || ''} />
+          <img
+            className={styles.Img}
+            src={`${pathName(src)}${picname(src)}.webp`}
+            alt={alt || ''}
+          />
         </>
       )}
     </picture>
