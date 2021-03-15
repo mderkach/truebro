f5711f54c9714c527ab9ed33610ae88f359cb5f3
Konstantin <serba.kostia@yandex.ru>
2021-03-15 13:29:50 +0300

    ubuntu_fix_path_2

---
+++ src/components/Table/TableHeader/TableHeader.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Icon from '/src/components/Icon/Icon';
+import '../TableRow/TableRow.scss';
+
+const TableHeader = (props) => {
+  const { headers, sortDirection, sortBy } = props;
+  return (
+    <>
+      {headers.map((elem) => (
+        <div
+          role="presentation"
+          key={elem.name}
+          data-key={elem.label}
+          className="table__cell"
+          style={{ textAlign: elem.align }}
+          onClick={() => sortBy(elem.label, elem.sortable)}
+        >
+          {elem.name}
+          {elem.sortable && <Icon cls={`table__sort ${sortDirection}`} name="sort-icon" />}
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default TableHeader;
